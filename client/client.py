73fcabaee3cdd00e14ee56d074da032173f9f913
Lasiagrostis <lasiagrostis@mail.ru>
2025-12-08 00:18:26 +0300

    Правки в proto и клиенте

--- client/client.py
+++ client/client.py
@@ -1,9 +1,17 @@
 import grpc
-import model_pb2, model_pb2_grpc
+from protos import model_pb2, model_pb2_grpc
 
 channel = grpc.insecure_channel("localhost:50051")
 stub = model_pb2_grpc.PredictionServiceStub(channel)
 
-request = model_pb2.PredictRequest(feature1="1")
-response = stub.Predict(request)
-print("Prediction:", response.prediction)
+print("----- Prediction Request -----")
+predict_request = model_pb2.PredictRequest(pclass=1.0)
+predict_response = stub.Predict(predict_request)
+
+print("Prediction:", predict_response.prediction, "Probability:", round(predict_response.confidence, 3))
+
+print("\n----- Health Check -----")
+health_request = model_pb2.Empty()
+health_response = stub.Health(health_request)
+print("Status:", health_response.status)
+print("Model version:", health_response.modelVersion)
--- server/server.py
+++ server/server.py
@@ -1,26 +1,34 @@
 import grpc
 from concurrent import futures
-import model_pb2, model_pb2_grpc
+from protos import model_pb2, model_pb2_grpc
 import joblib, pandas as pd
+import os
+
+MODEL_PATH = os.environ["MODEL_PATH"]
+MODEL_VERSION = os.environ["MODEL_VERSION"]
+PORT = os.environ["PORT"]
 
 class PredictionService(model_pb2_grpc.PredictionServiceServicer):
     def __init__(self):
-        self.model = joblib.load("model.pkl")
+        self.model = joblib.load(MODEL_PATH)
+        self.model_version = MODEL_VERSION
 
     def Predict(self, request, context):
         df = pd.DataFrame([{
-            "feature1": request.feature1
+            "Pclass": request.pclass
         }])
         pred = self.model.predict(df)[0]
-        return model_pb2.PredictResponse(prediction=str(pred))
+        proba = max(self.model.predict_proba(df)[0])
+
+        return model_pb2.PredictResponse(prediction=str(pred), confidence=float(proba))
 
     def Health(self, request, context):
-        return model_pb2.HealthResponse(status="ok")
+        return model_pb2.HealthResponse(status="ok", modelVersion=self.model_version)
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
     model_pb2_grpc.add_PredictionServiceServicer_to_server(PredictionService(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port(f'[::]:{PORT}')
     server.start()
     server.wait_for_termination()
 
